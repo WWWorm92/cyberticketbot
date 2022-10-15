ba3147b19ac9b5c4bb984414afb351967bd7edfb
WWWorm02 <yadanbryvko@gmail.com>
2022-10-15 06:32:26 +0300

    update 1

--- botticket.py
+++ botticket.py
@@ -1,26 +1,38 @@
 from email import message
 
 import telebot;
+import update as update
 from telebot import types
 import logging
 
 bot = telebot.TeleBot('5308126595:AAG08b3BIlDCtqBH4Iq8lNFVpLHA7rbjn5o')
+chat_id_user = '-1001784446207'  # id чата  с пользователем
+chat_id_self = '445283271'  # id чата с ботом
+chat_id_tickets = '-814258509 '  # id чата с поддержкой
 
 
-@bot.message_handler(content_types=['text', 'document', 'audio', 'photo'])
-def get_text_messages(message):
-    bool = 'no'
-    if message.text == "/ticket":
-        bot.send_message(message.from_user.id)  # КНОПКИ
-    log_msg(message)
+# @bot.message_handler(content_types=['text', 'document', 'audio', 'photo'], commands=['start', 'ticket'])
+# def get_text_messages(message):
+#     if message.text == "/ticket":
+#         bot.send_message(chat_id=chat_id_user,
+#                          text='Введите номер ПК и описание проблемы')  # запрос сообщения в чате с пользователем
+@bot.message_handler(func=lambda message2: True)
+def message_receive(message_user):
+    if chat_id_user:
+        bot.send_message(chat_id=chat_id_user,
+                        text=message_user.text)  # пересылка ответа пользователя в чат с поддержкой
+    log_msg(message_user)
+    print('USER')
+    message_send(message_user)
+@bot.message_handler(func=lambda message1: True)
+def message_send(message_ticket):
+    if chat_id_tickets:
+        bot.send_message(chat_id=chat_id_tickets, text=message_ticket.text)  # пересылка ответа поддержки в чат с пользователем
+    log_msg(message_ticket)
+    print('TICKET')
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_worker(call):
-    if call.data == "close":  # call.data это callback_data, которую мы указали при объявлении кнопки
-        # код сохранения данных, или их обработки
-        bot.delete_message(call.message.chat.id, call.message.message_id - 1)
-        #bot.delete_message(call.message.chat.id, call.message.message_id)
+
 
 
 def log_msg(msg):
