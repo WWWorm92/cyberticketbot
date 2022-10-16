312f52a4830a3952f470bad8ce64859efee392e0
СyberX Admin <yadanbryvko@gmail.com>
2022-10-16 21:09:11 +0300

    shit

--- botticket.py
+++ botticket.py
@@ -10,12 +10,12 @@ conn = sqlite3.connect('db/botDB.db', check_same_thread=False)
 cursor = conn.cursor()
 
 
-def db_table_val(user_id: int, user_name: str, user_surname: str, username: str, message_id: int, message_text: str):
-    cursor.execute(
-        'INSERT INTO botlog (user_id, user_name, user_surname, username,message_id,message_text) VALUES (?, ?, ?, ?, '
-        '?, ?)',
-        (user_id, user_name, user_surname, username, message_id, message_text))
-    conn.commit()
+# def db_table_val(user_id: int, user_name: str, user_surname: str, username: str, message_id: int, message_text: str):
+#     cursor.execute(
+#         'INSERT INTO botlog (user_id, user_name, user_surname, username,message_id,message_text) VALUES (?, ?, ?, ?, '
+#         '?, ?)',
+#         (user_id, user_name, user_surname, username, message_id, message_text))
+#     conn.commit()
 
 
 @bot.message_handler(content_types=['text', 'document', 'audio', 'photo'], commands=['ticket', 'help', 'start'])
@@ -27,34 +27,52 @@ def get_text_messages(message):
         bot.send_message(message.chat.id, text='Неверная команда,введите /help для отображения списка команд')
 
 
+# @bot.message_handler(content_types=['text', 'document', 'audio', 'photo'])
+# def message_user(msgu):
+#     if msgu.chat.id == int(chat_id_user):
+#         bot.send_message(chat_id=chat_id_tickets, text=str(
+#             f'{msgu.from_user.username}\n') + msgu.text)  # пересылка ответа пользователя в чат с поддержкой
+#     elif msgu.chat.id == int(chat_id_tickets):
+#         bot.send_message(chat_id=chat_id_user, text=msgu.text)  # пересылка ответа поддержки в чат с пользователем
+#     log_msg(msgu)
+lol=0
 @bot.message_handler(content_types=['text', 'document', 'audio', 'photo'])
 def message_user(msgu):
     if msgu.chat.id == int(chat_id_user):
         bot.send_message(chat_id=chat_id_tickets, text=str(
             f'{msgu.from_user.username}\n') + msgu.text)  # пересылка ответа пользователя в чат с поддержкой
-    elif msgu.chat.id == int(chat_id_tickets):
-        bot.send_message(chat_id=chat_id_user, text=msgu.text)  # пересылка ответа поддержки в чат с пользователем
-    log_msg(msgu)
-
-
-def log_msg(msg):
-    mcfn = str(msg.chat.first_name)
-    mcln = str(msg.chat.last_name)
-    mcid = str(msg.chat.id)
-    muid = str(msg.from_user.id)
-    msgid = msg.message_id
-    username = msg.from_user.username
-    print('=================================================================================')
-    if mcln != 'None':
-        print(f"Message from {mcfn} {mcln}  ,Chat iD: {mcid} ,User ID: {muid} ,User Name:{username}")
-        db_table_val(user_id=int(muid), user_name=mcfn, user_surname=mcln, username=username, message_id=msgid,
-                     message_text=msg.text)
+        lol = msgu.from_user.id
     else:
-        print(f"Message from {mcfn} ,Chat iD: {mcid} ,User ID: {muid},User Name:{username}")
-        db_table_val(user_id=int(muid), user_name=mcfn, user_surname=mcln, username=username, message_id=msgid,
-                     message_text=msg.text)
-    print('Text:', str(msg.text))
-    print('=================================================================================')
+        message_ticket(lol)
+    print('U')
+    # log_msg(msgu)
+
+
+@bot.message_handler(content_types=['text', 'document', 'audio', 'photo'])
+def message_ticket(msgu):
+    if msgu.chat.id == int(chat_id_tickets):
+        bot.send_message(chat_id=message_user(msgu.from_user.id),
+                         text=msgu.text)  # пересылка ответа поддержки в чат с пользователем
+    print('T')
+
+# def log_msg(msg):
+#     mcfn = str(msg.chat.first_name)
+#     mcln = str(msg.chat.last_name)
+#     mcid = str(msg.chat.id)
+#     muid = str(msg.from_user.id)
+#     msgid = msg.message_id
+#     username = msg.from_user.username
+#     print('=================================================================================')
+#     if mcln != 'None':
+#         print(f"Message from {mcfn} {mcln}  ,Chat iD: {mcid} ,User ID: {muid} ,User Name:{username}")
+#         db_table_val(user_id=int(muid), user_name=mcfn, user_surname=mcln, username=username, message_id=msgid,
+#                      message_text=msg.text)
+#     else:
+#         print(f"Message from {mcfn} ,Chat iD: {mcid} ,User ID: {muid},User Name:{username}")
+#         db_table_val(user_id=int(muid), user_name=mcfn, user_surname=mcln, username=username, message_id=msgid,
+#                      message_text=msg.text)
+#     print('Text:', str(msg.text))
+#     print('=================================================================================')
 
 
 try:
