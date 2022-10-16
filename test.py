342e9bad463f21b71449d098d640140974b2d342
PC-18 <yadanbryvko@gmail.com>
2022-10-16 22:09:54 +0300

    shit 2

--- test.py
+++ test.py
@@ -9,6 +9,13 @@ conn = sqlite3.connect('db/botDB.db', check_same_thread=False)
 cursor = conn.cursor()
 
 
+def db_table_val(user_id: int, user_name: str, user_surname: str, username: str, message_id: int, message_text: str):
+    cursor.execute(
+        'INSERT INTO botlog (user_id, user_name, user_surname, username,message_id,message_text) VALUES (?, ?, ?, ?, ?, ?)',
+        (user_id, user_name, user_surname, username, message_id, message_text))
+    conn.commit()
+
+
 @bot.message_handler(content_types=['text', 'document', 'audio', 'photo'], commands=['ticket', 'help', 'start'])
 def get_text_messages(message):
     if message.text == '/ticket' or '/ticket@cyberxproblems_bot':
@@ -20,16 +27,37 @@ def get_text_messages(message):
 
 @bot.message_handler(content_types=['text', 'document', 'audio', 'photo'])
 def message_user(message):
-    #print(type(message.chat.id), type(chat_id_tickets), bool(message.reply_to_message))
-    #print(message.chat.id == chat_id_tickets, bool(message.reply_to_message))
+    # print(type(message.chat.id), type(chat_id_tickets), bool(message.reply_to_message))
+    # print(message.chat.id == chat_id_tickets, bool(message.reply_to_message))
     if message.chat.id == int(chat_id_user):
         bot.send_message(chat_id=chat_id_tickets, text=str(
             f'@{message.from_user.username}\n') + message.text)  # пересылка ответа пользователя в чат с поддержкой
     elif message.chat.id == int(chat_id_tickets) and message.reply_to_message:
-        #print('replay')
-        #print(message.reply_to_message.text.split('\n')[0] + message.text)
+        # print('replay')
+        # print(message.reply_to_message.text.split('\n')[0] + message.text)
         replay: str = message.reply_to_message.text.split('\n')[0] + '\n' + message.text
         bot.send_message(chat_id=int(chat_id_user), text=replay)  # пересылка ответа поддержки в чат с пользователем
+    print(log_msg(message))
+
+
+def log_msg(msg):
+    mcfn = str(msg.chat.first_name)
+    mcln = str(msg.chat.last_name)
+    mcid = str(msg.chat.id)
+    muid = str(msg.from_user.id)
+    msgid = msg.message_id
+    username = msg.from_user.username
+    print('=================================================================================')
+    if mcln != 'None':
+        print(f"Message from {mcfn} {mcln}  ,Chat iD: {mcid} ,User ID: {muid} ,User Name:{username}")
+        db_table_val(user_id=int(muid), user_name=mcfn, user_surname=mcln, username=username, message_id=msgid,
+                     message_text=msg.text)
+    else:
+        print(f"Message from {mcfn} ,Chat iD: {mcid} ,User ID: {muid},User Name:{username}")
+        db_table_val(user_id=int(muid), user_name=mcfn, user_surname=mcln, username=username, message_id=msgid,
+                     message_text=msg.text)
+    print('Text:', str(msg.text))
+    print('=================================================================================')
 
 
 try:
